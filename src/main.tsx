8e18564dcbe5897c74be335a16ec032a15f341dd
sansai-SNCT <syunzo1115@icloud.com>
2022-10-01 11:26:14 +0900

    [update] rootを修正

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import GlobalStyles from './styles/GlobalStyles'
 import App from './App'
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
+const container = document.querySelector('#root') as HTMLElement
+const root = createRoot(container)
+
 root.render(
   <React.StrictMode>
     <GlobalStyles />
