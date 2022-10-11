e2475afcd67f9146ae85a34eaba859818a815ee8
sansai-SNCT <syunzo1115@icloud.com>
2022-10-11 17:41:03 +0900

    [add] loadingコンポーネントの実装

---
+++ src/tests/Loading/Loading.test.tsx
@@ -0,0 +1,14 @@
+import { composeStories } from '@storybook/testing-react'
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import React from 'react'
+import * as stories from '../../stories/Loading.stories'
+
+const { Default } = composeStories(stories)
+
+describe('(components/common) Loading', () => {
+  test('take snap shot', () => {
+    const { container } = render(<Default />)
+    expect(container).toMatchSnapshot()
+  })
+})
