28d85956b1e65894c847e5988f46989a287284cf
sansai-SNCT <syunzo1115@icloud.com>
2022-10-19 20:44:07 +0900

    [add] Textコンポーネントの実装

---
+++ src/components/common/Text.tsx
@@ -0,0 +1,5 @@
+import tw from 'twin.macro'
+
+const Text = tw.p`text-base font-medium select-none`
+
+export { Text }
---
+++ src/stories/common/Text.stories.tsx
@@ -0,0 +1,26 @@
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react'
+import type { ComponentPropsWithoutRef } from 'react'
+import { Text } from '../../components/common/Text'
+
+type T = typeof Text
+type Story = ComponentStoryObj<T>
+type Meta = ComponentMeta<T>
+
+const args: ComponentPropsWithoutRef<T> = {
+  children: 'This is test',
+}
+
+export default {
+  args,
+  argTypes: {
+    children: {
+      description: 'Context of text',
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  component: Text,
+} as Meta
+
+export const Default: Story = {}
