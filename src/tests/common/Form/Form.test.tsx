fce4db2b8a936c763c2e54599fed78ffbcaa79bf
sansai-SNCT <syunzo1115@icloud.com>
2022-10-21 01:00:47 +0900

    [add] inputテストを追加

--- src/tests/common/Form/Form.test.tsx
+++ src/tests/common/Form/Form.test.tsx
@@ -1,6 +1,6 @@
 import { composeStories } from '@storybook/testing-react'
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import React from 'react'
 import * as stories from '../../../stories/common/Form.stories'
 
@@ -10,5 +10,17 @@ describe('(components/common) Form', () => {
   test('take snap shot', () => {
     const { container } = render(<Default />)
     expect(container).toMatchSnapshot()
+  })
+})
+
+describe('(components/common) Form', () => {
+  test('input test', async () => {
+    render(<Default />)
+    fireEvent.change(screen.getByRole('searchbox'), {
+      target: { value: "';delete from user_table;" },
+    })
+    expect(
+      await screen.getByDisplayValue('&#x27;;delete from user_table;'),
+    ).toBeInTheDocument()
   })
 })
