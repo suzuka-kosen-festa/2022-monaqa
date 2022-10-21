afa950fe8567cbf097326a02133e8ac55aa64e5b
sansai-SNCT <syunzo1115@icloud.com>
2022-10-21 19:56:20 +0900

    [move] 場所を変更

--- src/stories/common/Loading.stories.tsx
+++ src/stories/common/Loading.stories.tsx
@@ -1,5 +1,5 @@
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react'
-import { Loading } from '../components/common/Loading'
+import { Loading } from '../../components/common/Loading'
 
 type T = typeof Loading
 type Story = ComponentStoryObj<T>
