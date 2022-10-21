1d46bd0bc264989dc70bd056a9b0565525531be8
sansai-SNCT <syunzo1115@icloud.com>
2022-10-21 19:58:05 +0900

    [remove] 不要なサンプルを削除

--- src/components/Sample.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import tw from 'twin.macro'
-
-const SampleContainar = tw.div`bg-gray-400`
-
-export const Sample = (): JSX.Element => (
-  <SampleContainar>HelloWorld</SampleContainar>
-)
