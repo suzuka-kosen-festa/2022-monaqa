4a09a477b5a5c634f201c641f0ffbf063e6c98fc
sansai-SNCT <syunzo1115@icloud.com>
2022-10-21 14:24:40 +0900

    [fix] errorを取り除いた

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,20 +2,7 @@ const react = require('@vitejs/plugin-react')
 
 module.exports = {
   stories: ['../src/stories/**/*.stories.@(ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-    '@storybook/addon-a11y',
-  ],
+  addons: [],
   staticDirs: ['../public'],
   framework: '@storybook/react',
   core: {
