1453cb012f52d34b5e70095c12026bae10ce77d5
sansai-SNCT <syunzo1115@icloud.com>
2022-10-25 00:03:05 +0900

    [tukareta] apiがpagesに...

--- src/components/utils/API.ts
+++ src/components/utils/API.ts
@@ -1,5 +1,3 @@
-import apiClient from './axios'
-
 /* 本実装ではAPIを叩くときに変換する */
 export const escapeHTML = (text: string): string =>
   text
@@ -7,44 +5,3 @@ export const escapeHTML = (text: string): string =>
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#x27;')
-
-// テスト用
-export const fetch = (): void => {
-  apiClient
-    .get('/health/http')
-    .then(res => {
-      console.log(res)
-      return res.data
-    })
-    .catch(err => {
-      console.log(err)
-      return []
-    })
-}
-
-// タイムスタンプ処理
-export const timeStamp = (uuid: string): void => {
-  apiClient
-    .get(`/admin/check/${uuid}`)
-    .then(res => {
-      console.log(res)
-      return res.data
-    })
-    .catch(err => {
-      console.log(err)
-      return []
-    })
-}
-
-// かな検索用API
-export const searchName = (kana: string): void => {
-  apiClient
-    .get(`/student/${escapeHTML(kana)}`)
-    .then(res => {
-      console.log(res.data)
-    })
-    .catch(err => {
-      console.log(err)
-      return []
-    })
-}
