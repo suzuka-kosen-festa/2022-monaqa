2c6f11a5820272d0824cc719760c41cfb1e5fc00
sansai-SNCT <syunzo1115@icloud.com>
2022-10-24 23:10:26 +0900

    [add] APIを実装及び諸々の調整

--- src/components/pages/QR.tsx
+++ src/components/pages/QR.tsx
@@ -1,45 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import type { FC } from 'react'
 import tw from 'twin.macro'
+
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+import apiClient from '../utils/axios'
 import { QRCodeReader } from '../common/QRCodeReader'
 import { Text } from '../common/Text'
 
-const Box = tw.div``
 const QRContainer = tw.div`flex flex-col justify-center items-center`
 
-/*
-  TODO: 
-  QRからはuuidが読み取れる。
-  uuidをkeyとしてGETメソッドを叩くとタイムスタンプが押される処理を書く。
-  responce 200の場合 -> 中身がnullでなければ成功アラート nullならエラーを返す
- responece 500の場合 -> エラーを返す。
-*/
-
-const notify = (code: string) => {
-  toast.success(code)
-}
-
 const QR: FC = () => {
-  const [Scanflug, setScanflug] = useState(false)
-
+  const timeStamp = (uuid: string): void => {
+    apiClient
+      .get(`/admin/check/${uuid}`)
+      .then(res => {
+        if (res.data != null)
+          toast.success(`${res.data.name}さんの入場を記録しました`)
+        else toast.error('error')
+      })
+      .catch(err => {
+        toast.error(err.message)
+      })
+  }
   return (
     <QRContainer>
       <QRCodeReader
         onReadQRCode={result => {
-          setScanflug(true)
-          notify(result.getText())
+          timeStamp(result.getText())
         }}
       />
-      <Box>
-        {Scanflug ? (
-          <ToastContainer position="bottom-center" />
-        ) : (
-          <Text>QRコードをかざしてください</Text>
-        )}
-      </Box>
+
+      <ToastContainer position="bottom-center" />
+
+      <Text>QRコードをかざしてください</Text>
     </QRContainer>
   )
 }
