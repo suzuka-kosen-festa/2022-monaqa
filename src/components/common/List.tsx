96c94dc270136ca0f3588e6b94edfe586daa39b5
sansai-SNCT <syunzo1115@icloud.com>
2022-10-25 00:00:52 +0900

    [update] APIからデータを取得しタイムスタンプを実装

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import type { FC } from 'react'
 import tw from 'twin.macro'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import apiClient from '../utils/axios'
 import { Modal } from './Modal'
@@ -15,20 +17,22 @@ interface ListInterface {
 const DataTable = tw.ul`text-center w-full lg:w-1/2 `
 const ListItem = tw.li`flex items-center justify-between border-2 mb-1  p-4 border-gray-400`
 
-const List: FC<ListInterface> = ({ data, data1, buttonText1, buttonText2 }) => {
-  const timeStamp = (uuid: string): void => {
-    apiClient
-      .get(`/guest/check/${uuid}`)
-      .then(res => {
-        console.log(res.data)
-      })
-      .catch(err => {
-        console.log(err)
-        return []
-      })
-  }
+const timeStamp = (uuid: string): void => {
+  apiClient
+    .get(`/admin/check/${uuid}`)
+    .then(res => {
+      if (res.data != null)
+        toast.success(`${res.data.name}さんの入場を記録しました`)
+      else toast.error('error')
+    })
+    .catch(err => {
+      toast.error(err.message)
+    })
+}
 
-  return (
+const List: FC<ListInterface> = ({ data, data1, buttonText1, buttonText2 }) => (
+  <>
+    {' '}
     <DataTable>
       {data1.map((item, index) => (
         <ListItem key={item}>
@@ -36,15 +40,16 @@ const List: FC<ListInterface> = ({ data, data1, buttonText1, buttonText2 }) => {
           <Modal
             buttonText1={buttonText1}
             buttonText2={buttonText2}
-            name="name"
+            name={item}
             onClick={() => timeStamp(data[index].guestId)}
           >
             入場記録
           </Modal>
         </ListItem>
       ))}
-    </DataTable>
-  )
-}
+    </DataTable>{' '}
+    <ToastContainer position="bottom-center" />
+  </>
+)
 
 export { List }
