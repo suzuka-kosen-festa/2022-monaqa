b231a67e55ff3ca0b051d660b24eecbfab41c15c
sansai-SNCT <syunzo1115@icloud.com>
2022-10-26 12:26:27 +0900

    [update] modalのstateを外で管理する

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import type { FC } from 'react'
 import tw from 'twin.macro'
 
@@ -6,8 +6,9 @@ import { Button } from './Button'
 import { Text } from './Text'
 
 interface ModalInterface {
-  children: React.ReactNode
   name: string
+  showFlag: boolean
+  setShowFlag: React.Dispatch<React.SetStateAction<boolean>>
   buttonText1: string
   buttonText2: string
   onClick: () => void
@@ -18,36 +19,30 @@ const ModalBox = tw.div`flex items-center min-h-screen px-4 py-8`
 const DiaLog = tw.div`relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg text-center space-y-4 space-x-4`
 
 const Modal: FC<ModalInterface> = ({
-  children,
   name,
   buttonText1,
   buttonText2,
   onClick,
+  showFlag,
+  setShowFlag,
 }) => {
-  const [showModal, setShowModal] = useState(false)
-  /* Todo: create portalやる const root = document.getElementById('modal') */
-  return (
-    <>
-      <Button buttonKey="accept" onClick={() => setShowModal(true)}>
-        {children}
-      </Button>
-      {showModal ? (
-        <ModalArea>
-          <ModalBox>
-            <DiaLog>
-              <Text>{name}さんの入場を記録しますか？</Text>
-              <Button buttonKey="reject" onClick={() => setShowModal(false)}>
-                {`${buttonText1}`}
-              </Button>
-              <Button buttonKey="accept" onClick={onClick}>
-                {`${buttonText2}`}
-              </Button>
-            </DiaLog>
-          </ModalBox>
-        </ModalArea>
-      ) : null}
-    </>
-  )
+  const closeModal = () => setShowFlag(false)
+  return showFlag ? (
+    <ModalArea>
+      <ModalBox>
+        <DiaLog>
+          <Text>{name}さんの入場を記録しますか？</Text>
+          <Button buttonKey="reject" onClick={() => closeModal()}>
+            {`${buttonText1}`}
+          </Button>
+          <Button buttonKey="accept" onClick={onClick}>
+            {`${buttonText2}`}
+          </Button>
+        </DiaLog>
+      </ModalBox>
+    </ModalArea>
+  ) : null
 }
+/* Todo: create portalやる const root = document.getElementById('modal') */
 
 export { Modal }
